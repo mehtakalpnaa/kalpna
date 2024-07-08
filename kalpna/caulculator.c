539ea91585ac67dfb16f69310e985778c9151814
mehtakalpnaa <kalpnamehtak@gmail.com>
2024-07-08 15:37:22 +0530

    devide and multiply

---
+++ kalpna/caulculator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int multiply(int a, int b);
+int devide(int a, int b);
+
+
+
+
+int multiply(int a, int b)
+{
+    return a * b;
+}
+int devide(int a, int b)
+{
+    return a / b;
+}
