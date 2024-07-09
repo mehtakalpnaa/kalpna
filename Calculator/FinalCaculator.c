c4ad73b48a1b9a4025e87d0f39fe10663b757d58
shifagayas <shifagayas95@gmail.com>
2024-07-09 12:44:10 +0530

    Final Complete Calculator

---
+++ Calculator/FinalCaculator.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int multiply(int a, int b);
+int devide(int a, int b);
+
+void main()
+{
+    int num1,num2,result;
+    char ch;
+
+    
+
+        do{
+            printf("\nEnter a number :");
+            scanf("%d",&num1);
+            printf("\nEnter a number :");
+            scanf("%d",&num2);
+
+            printf("\nEnter + for Addition");
+            printf("\nEnter - for Subtraction");
+            printf("\nEnter * for Multiplication");
+            printf("\nEnter / for Division");
+            printf("\n0 for Exit\n");
+
+            printf("\nEnter a choice : ");
+            scanf(" %c",&ch);
+
+            switch(ch)
+        {
+            case '+':
+                
+                printf("%d",result);
+                break;
+            case '-':
+                
+                printf("%d",result);
+                break;
+            case '*':
+                result=multiply(num1,num2);
+                printf("%d",result);
+                break;
+            case '/':
+                result=devide(num1,num2);
+                printf("%d",result);
+                break;
+            case 0:
+                break;    
+            default:
+                printf("\nplease enter valid oprater");
+        }
+
+        }while(ch!=0);
+
+  
+    
+        
+}
+int multiply(int a, int b)
+{
+    return a * b;
+}
+int devide(int a, int b)
+{
+    return a / b;
+}
